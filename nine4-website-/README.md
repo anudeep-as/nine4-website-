cc4cc88e3d3de4a53251278914c3b10e9e0f43c9
Anudeep AS <anudeep.a.s2005@gmail.com>
2025-07-15 10:55:55 +0530

    Initial commit with updated website

---
+++ nine4-website-/README.md
@@ -0,0 +1,1 @@
+# nine4-website-
---
+++ nine4-website-/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],  // 👈 tells Tailwind to scan all .html files in the root
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
