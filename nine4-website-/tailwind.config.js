00f72178321e28b5bdcbb280dac789f1426508ab
Anudeep AS <anudeep.a.s2005@gmail.com>
2025-07-15 11:22:15 +0530

    Remove unwanted folder from repo

--- nine4-website-/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html"],  // 👈 tells Tailwind to scan all .html files in the root
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
